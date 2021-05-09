fd36b55a0fa75a9ec71728b4b26e3289b022fb02
ltzone <ltzhou@sjtu.edu.cn>
2021-05-09 09:28:39 +0800

    frontend demo

--- frontend/src/views/distributed/Monitor.vue
+++ frontend/src/views/distributed/Monitor.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="monitor">
+  <v-container>
     <h1>This is monitor page</h1>
-  </div>
+
+    <v-textarea
+      name="input-7-1"
+      label="Default style"
+      :value="running_tasks[0].name"
+      hint="Hint text"
+    ></v-textarea>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    name: 'Application',
+    components: {},
+    data () {
+      return {
+        running_tasks: [
+          {
+            id: 1,
+            name: 'Map Reduce',
+            user: 'Tony Zhou',
+            description: 'Map Reducing',
+            eta: '70min',
+          },
+        ],
+      }
+    },
+  }
+</script>
