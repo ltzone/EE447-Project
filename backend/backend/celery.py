9816831d2f04b8c4033e1f4ba7b45bd1e29216fe
ltzone <ltzhou@sjtu.edu.cn>
2021-05-09 15:23:09 +0800

    tmp

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -21,3 +21,22 @@ app.autodiscover_tasks()
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
+@app.task(name='ic.mapper')
+def mapper():
+    #split your problem in embarrassingly parallel maps 
+    maps = [map.s(), map.s(), map.s(), map.s(), map.s(), map.s(), map.s(), map.s()]
+    #and put them in a chord that executes them in parallel and after they finish calls 'reduce'
+    mapreduce = app.chord(maps)(reduce.s())    
+    return "{0} mapper ran on {1}".format(app.current_task.request.id, app.current_task.request.hostname)
+
+@app.task(name='ic.map')
+def map():
+    #do something useful here
+    import time
+    time.sleep(10.0)
+    return "{0} map ran on {1}".format(app.current_task.request.id, app.current_task.request.hostname)
+
+@app.task(name='ic.reduce')
+def reduce(results):
+    #put the maps together and do something with the results
+    return "{0} reduce ran on {1}".format(app.current_task.request.id, app.current_task.request.hostname)
