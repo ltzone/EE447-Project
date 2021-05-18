991f93767358b98468577e1608d799f94b72a81b
Zsi-r <949480148@qq.com>
2021-05-18 12:52:44 +0800

    add comments in urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,14 +5,22 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # 得到运行结果 TaskResult列表
     path('get', views.task_result_list),
+
+    # 得到task运行实时状态 TaskState列表
     path('tasks', views.task_list),
+
+    # 仅用于调试的简单任务，
     path('test', views.ctest),
     path('sleep', views.sleep),
+
+     # 提交任务
     path('submit', views.submit_task),
-    path('mapreduce', views.submit_map_reduce),
-    path('filtertask', views.filter_task),
-    path('filterresult', views.filter_task_result),
-    path('filterreduce', views.filter_reduce),
-    path('tasks2', views.task_list_with_customtaskname)
+    path('mapreduce', views.submit_map_reduce), # 提交mapreduce任务
+    path('filtertask', views.filter_task), # 根据request.data.get("state")筛选特定state的TaskState
+    path('filterresult', views.filter_task_result), # 根据request.data.get("state")筛选特定state的TaskResult
+    path('filterreduce', views.filter_reduce), # 筛选出reduce任务的TaskState，无需参数
+    path('tasks2', views.task_list_with_customtaskname) # 根据request.data.get("custom_task_name")筛选特定自定义用户名的TaskState
 ]
